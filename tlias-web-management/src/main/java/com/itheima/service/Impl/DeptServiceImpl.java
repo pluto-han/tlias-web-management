d427220325c5d1326cabb19ac01595ed3950dd0d
pluto-han <1514142997@qq.com>
2025-01-24 16:07:37 +0800

    项目环境搭建

---
+++ tlias-web-management/src/main/java/com/itheima/service/Impl/DeptServiceImpl.java
@@ -0,0 +1,9 @@
+package com.itheima.service.Impl;
+
+import com.itheima.service.DeptService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+
+}
