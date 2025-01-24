9f23b36a79782a414fce7863be68b6441cb001a7
pluto-han <1514142997@qq.com>
2025-01-25 02:11:26 +0800

    增加日志记录技术

--- tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
+++ tlias-web-management/src/main/java/com/itheima/controller/DeptController.java
@@ -3,11 +3,15 @@ package com.itheima.controller;
 import com.itheima.pojo.Dept;
 import com.itheima.pojo.Result;
 import com.itheima.service.DeptService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequestMapping("/depts")
 @RestController
 public class DeptController {
@@ -17,7 +21,7 @@ public class DeptController {
     // 查询全部部门名称
     @GetMapping
     public Result list() {
-        System.out.println("查询全部部门数据");
+        log.info("查询全部部门数据");
         List<Dept> deptList = deptService.findAll();
 
         return Result.success(deptList);
@@ -27,7 +31,7 @@ public class DeptController {
     @DeleteMapping
     // 前端传递的请求参数名和定义的形参名字一样
     public Result delete(Integer id) {
-        System.out.println("根据ID删除部门" + id);
+        log.info("根据ID删除部门: {}", id);
         deptService.deleteById(id);
 
         return Result.success();
@@ -36,7 +40,7 @@ public class DeptController {
     // 新增部门
     @PostMapping
     public Result add(@RequestBody Dept dept) {
-        System.out.println("新增部门" + dept);
+        log.info("新增部门: {}", dept);
         deptService.add(dept);
 
         return Result.success();
@@ -45,7 +49,7 @@ public class DeptController {
     // 根据id查询部门（查询回显）
     @GetMapping("/{id}")
     public Result getInfo(@PathVariable Integer id) {
-        System.out.println("根据id查询部门:" + id);
+        log.info("根据id查询部门数据: {}", id);
         Dept dept = deptService.getById(id);
 
         return Result.success(dept);
@@ -54,7 +58,7 @@ public class DeptController {
     // 修改部门
     @PutMapping
     public Result update(@RequestBody Dept dept) {
-        System.out.println("修改部门" + dept);
+        log.info("修改部门数据: {}", dept);
         deptService.update(dept);
 
         return Result.success();
