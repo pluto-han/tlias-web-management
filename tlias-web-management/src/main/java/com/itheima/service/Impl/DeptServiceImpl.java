3e2bdadaaf146ff43174a67f29a6c6ca7691ffed
pluto-han <1514142997@qq.com>
2025-01-25 01:16:58 +0800

    部门管理-CRUD开发完成

--- tlias-web-management/src/main/java/com/itheima/service/Impl/DeptServiceImpl.java
+++ tlias-web-management/src/main/java/com/itheima/service/Impl/DeptServiceImpl.java
@@ -1,9 +1,47 @@
 package com.itheima.service.Impl;
 
+import com.itheima.mapper.DeptMapper;
+import com.itheima.pojo.Dept;
 import com.itheima.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class DeptServiceImpl implements DeptService {
+    @Autowired
+    private DeptMapper deptMapper;
 
+    @Override
+    public List<Dept> findAll() {
+        return deptMapper.findAll();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        deptMapper.deleteById(id);
+    }
+
+    @Override
+    public void add(Dept dept) {
+        // 补全属性
+        dept.setCreateTime(LocalDateTime.now());
+        dept.setUpdateTime(LocalDateTime.now());
+
+        // 调用mapper接口方法
+        deptMapper.insert(dept);
+    }
+
+    @Override
+    public Dept getById(Integer id) {
+        return deptMapper.getById(id);
+    }
+
+    @Override
+    public void update(Dept dept) {
+        dept.setUpdateTime(LocalDateTime.now());
+        deptMapper.update(dept);
+    }
 }
